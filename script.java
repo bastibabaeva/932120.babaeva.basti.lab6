8698ef39c1f49a12c36ccbb6f7d8002f22084ad4
Basti <basti.babaeva@yandex.ru>
2024-10-18 16:06:21 +0700

    создание контейнеров для картинок; подключение java-методов для работы с кнопками; добавление картинки с собачкой
    добавление форматов для кнопок с картинками

--- script.java
+++ script.java
@@ -1,10 +1,13 @@
 
 function LeftColumn()
 {
-
+    const leftImage=document.querySelector('.left-image');
+    const rightImage=document.querySelector('.right-image');
     const leftContainer = document.querySelector('.left-image-container');
     const rightContainer = document.querySelector('.right-image-container');
 
+    leftImage.style.display="block";
+    rightImage.style.display="none";
     leftContainer.style.width="100%";
     rightContainer.style.width="0%";
 
@@ -12,17 +15,25 @@ function LeftColumn()
 
 function MiddleColumn()
 {
+    const leftImage=document.querySelector('.left-image');
+    const rightImage=document.querySelector('.right-image');
     const leftContainer = document.querySelector('.left-image-container');
     const rightContainer = document.querySelector('.right-image-container');
 
+    leftImage.style.display="block";
+    rightImage.style.display="block";
     leftContainer.style.width="100%";
     rightContainer.style.width="100%";
 }
 
 function RightColumn(){
+    const leftImage = document.querySelector('.left-image');
+    const rightImage = document.querySelector('.right-image');
     const leftContainer = document.querySelector('.left-image-container');
     const rightContainer = document.querySelector('.right-image-container');
 
+    rightImage.style.display="block";
+    leftImage.style.document="none";
     leftContainer.style.width="0%";
     rightContainer.style.width="100%";
 }
